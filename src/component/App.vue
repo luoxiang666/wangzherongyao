e7fa027c6d9e3340bb7fab9a73a07d5d5b0eb639
luoxiang <1041472327@qq.com>
2018-01-14 16:53:10 +0800

    安装的基本环境

---
+++ src/component/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        公共头部
+        <router-view></router-view>
+        公共底部
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
